bc6b277b3db4665696651c7fdaa27044cc9231fe
fsh1201 <54531652+fsh1201@users.noreply.github.com>
2024-01-03 11:55:51 +0800

    Update spin_axis.c

--- spin_axis.c
+++ spin_axis.c
@@ -1,3 +1,27 @@
+/* This program calls the program period_scan to look for the best spin state, using multi-threads.
+
+   syntax:
+   spin_axis lcs input_period_scan out_periods
+*/
+
+/*
+Copyright (C) 2023  Shuai Feng
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include <stdlib.h> 
 #include <stdio.h>
 #include <math.h>
@@ -12,7 +36,7 @@ int main(int argc, char* argv[])
 {
 	if (argc < 2)
 	{
-		printf("spin_axis lc input_period_scan out_periods\n");
+		printf("spin_axis lcs input_period_scan out_periods\n");
 		exit(0);
 	}
 	int nf;
